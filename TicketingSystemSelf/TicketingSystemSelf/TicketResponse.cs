affc1ef6ad729a2ef193a3983be1c30a8edf9ab0
Shabbir7999 <114503606+Shabbir7999@users.noreply.github.com>
2023-03-07 19:19:00 +0500

    pagination

---
+++ TicketingSystemSelf/TicketingSystemSelf/TicketResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace TicketingSystemSelf
+{
+    public class TicketResponse
+    {
+
+        public List<Ticket> Tickets { get; set; } = new List<Ticket>();
+
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+
+    }
+}
