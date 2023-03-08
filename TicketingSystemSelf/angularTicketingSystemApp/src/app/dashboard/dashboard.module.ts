216540ae3be403835d742ab92f7178c76c90856b
Muhammad Zubair <zubayrahmad0308@gmail.com>
2023-03-08 12:32:11 +0500

    Working on Pagination 04

--- TicketingSystemSelf/angularTicketingSystemApp/src/app/dashboard/dashboard.module.ts
+++ TicketingSystemSelf/angularTicketingSystemApp/src/app/dashboard/dashboard.module.ts
@@ -5,7 +5,7 @@ import { MainTableComponent } from './main-table/main-table.component';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
-// import {NgxPaginationModule} from 'ngx-pagination';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +18,7 @@ import {MatButtonModule} from '@angular/material/button';
     ScrollingModule,
     MatTableModule,
     MatButtonModule,
+    
     
   ],
   exports:[MainTableComponent]
